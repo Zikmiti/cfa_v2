eab8d2b45f1febe82b249ba30245d0473d5c8825
Zikmiti <41624992+Zikmiti@users.noreply.github.com>
2021-03-24 15:47:47 +1100

    fix migration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -106,9 +106,4 @@ class User implements UserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
-
-    public function __toString()
-    {
-        return $this->getUsername();
-    }
 }
